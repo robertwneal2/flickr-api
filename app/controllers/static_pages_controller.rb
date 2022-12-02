679117c0b3e3f7b45fdd012bf47060d9ca201266
Robert Neal <robertwneal2@gmail.com>
2022-12-02 09:15:37 -0800

    Controller and view changes to allow all photos of a user id input to be shown

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,27 @@
 class StaticPagesController < ApplicationController
   def index
-    @flickr = flickr = Flickr.new ENV["flickr_key"], ENV["flickr_secret"]
+    if search_params[:commit]
+      @flickr = flickr = Flickr.new ENV["flickr_key"], ENV["flickr_secret"]
+      if @flickr
+        @flickr_id = search_params[:flickr_id]
+        begin
+          photos = @flickr.people.getPhotos(user_id: @flickr_id)
+          @photo_urls = []
+          photos.each do |photo|
+            server_id = photo.server
+            id = photo.id
+            secret = photo.secret
+            url = "https://live.staticflickr.com/#{server_id}/#{id}_#{secret}.jpg"
+            @photo_urls << url
+          end
+        rescue
+          @user_not_found = true
+        end
+      end
+    end
+  end
+
+  def search_params
+    params.permit(:commit, :flickr_id)
   end
 end
